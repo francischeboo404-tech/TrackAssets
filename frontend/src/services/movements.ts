f3812afef865ed67b88b73eeeb2a5d6bc2aeeef7
Francis Chebo Ividah <fividah@gmail.com>
2026-07-04 18:50:01 +0300

    Added movement API service helper

---
+++ frontend/src/services/movements.ts
@@ -0,0 +1,21 @@
+import api from './api';
+
+export const issueItem = async (payload: any) => {
+  const res = await api.post('/movements/issue', payload);
+  return res.data;
+};
+
+export const returnItem = async (payload: any) => {
+  const res = await api.post('/movements/return', payload);
+  return res.data;
+};
+
+export const listIssues = async (params = {}) => {
+  const res = await api.get('/movements/issues', { params });
+  return res.data;
+};
+
+export const listReturns = async (params = {}) => {
+  const res = await api.get('/movements/returns', { params });
+  return res.data;
+};
